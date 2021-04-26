061e99b1160ee51c3f5d01985a822928da387071
pedro <pedrorogge@hotmail.com>
2021-04-26 19:29:21 -0300

    primeiro commit

---
+++ src/main/java/com/cadastro/cadastroAluno/models/cadastroAlunoApplication.java
@@ -0,0 +1,13 @@
+package com.cadastro.cadastroAluno.models;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class cadastroAlunoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(cadastroAlunoApplication.class, args);
+	}
+
+}
